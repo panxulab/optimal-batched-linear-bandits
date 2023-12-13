91ae47a26e4ce78ce16ddf24b5a18011da04f31a
Xuanfei Ren <xuanfeir@gmail.com>
2023-12-13 17:54:01 +0800

    folder_traverse

--- figures_main.py
+++ figures_main.py
@@ -1,25 +1,30 @@
 from plot_figures import *
 import numpy as np
+import os
 
-loaded_data = np.load("results\\end_k_3_d_2_eps_0.01.npz")
 
-loaded_data = np.load("results\\research_eps_0.01.npz")
-loaded_data = np.load("results\\random_k_3_d_2.npz")
+folder_path = "results"
 
-results = loaded_data["results"]
-batch_complexity = loaded_data["batch_complexity"]
-results_batch = loaded_data["results_batch"]
-arms = loaded_data["arms"]
-M = loaded_data["M"] #number of agents
-theta=loaded_data["theta"]
-horizon=loaded_data["horizon"]
-eps=loaded_data["epsilon"]
 
-my_plot=plot_figures(results,batch_complexity,results_batch,arms,M,theta,horizon,eps)
+file_list = os.listdir(folder_path)
 
-my_plot.plot_results()
 
-# my_plot.compute_batch_complexity()
+for filename in file_list:
+    loaded_data = np.load(os.path.join(folder_path, filename))
+    results = loaded_data["results"]
+    batch_complexity = loaded_data["batch_complexity"]
+    results_batch = loaded_data["results_batch"]
+    arms = loaded_data["arms"]
+    M = loaded_data["M"] #number of agents
+    theta=loaded_data["theta"]
+    horizon=loaded_data["horizon"]
+    eps=loaded_data["epsilon"]
 
-if M!= 3:
-    my_plot.plot_results_batch()
+    my_plot=plot_figures(results,batch_complexity,results_batch,arms,M,theta,horizon,eps)
+
+    my_plot.plot_results()
+
+    # my_plot.compute_batch_complexity()
+
+    if M!= 3:
+        my_plot.plot_results_batch()
